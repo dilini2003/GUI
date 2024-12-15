43a5de7482390a33bdeb437b90c7c9b7fb5438ce
dilini2003 <dilinidarshika71@gmail.com>
2024-12-16 02:20:51 +0530

    login create

--- frontend/src/Component/Navbar.js
+++ frontend/src/Component/Navbar.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import {FaBook, FaHeart, FaSearch, FaShoppingCart, FaUser, FaHome, FaList, FaTags, FaStore } from "react-icons/fa";
+import React, {useRef} from 'react';
+import {FaBook, FaHeart, FaSearch, FaShoppingCart, FaUser, FaHome, FaList, FaTags, FaStore, FaTimes } from "react-icons/fa";
 
 const Navbar = () =>{
+    const navRef = useRef();
+    const showNavbar=() =>{
+        navRef.current.classList.toggle("login-form");
+    }
+
     return(
         <div>
             <header className='header'>
@@ -15,7 +20,7 @@ const Navbar = () =>{
                 <div id="search-btn"><FaSearch /></div>
                 <a href='/#' ><FaHeart /></a>
                 <a href='/#' ><FaShoppingCart /></a>
-                <div id="login-btn" className='user-btn'><FaUser /></div>
+                <div id="login-btn" className='user-btn' onClick={showNavbar}><FaUser /></div>
             </div>
             </div>
             
@@ -35,6 +40,25 @@ const Navbar = () =>{
                 <a href="/#"><FaList /></a>
                 <a href="/#"><FaTags /></a>
                 <a href="/#"><FaStore /></a>
+            </div>
+
+            <div className="login-form-container" ref={navRef}>
+                <div id="close-login-btn" onClick={showNavbar}><FaTimes /></div>
+                <form action='#'>
+                    <h3>sign in</h3>
+                    <span>email</span>
+                    <input type="email" className='box' placeholder='enter your email'/>
+                    <span>password</span>
+                    <input type="password" className='box' placeholder='enter your password'/>
+                    <div className="check-box">
+                        <input type="checkbox" id='remember-me'/>
+                        <label htmlFor="">remember me</label>
+                    </div>
+                    <input type="submit" value="sign in" className='btn'/>
+                    <p>forgot password?<a href="\#">click here</a></p>
+                    <p>don't have an account?<a href="\#">create one</a></p>
+                    <p></p>
+                </form>
             </div>
         </div>
     )

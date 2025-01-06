46187239dcdbbaa89485ad1c17155001d863d4d3
dilini2003 <dilinidarshika71@gmail.com>
2025-01-07 00:57:08 +0530

    arrivals created

--- frontend/src/Component/Home.js
+++ frontend/src/Component/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from './Navbar';
 
-import { FaPlane, FaLock, FaHeadset, FaSearch, FaHeart, FaEye } from "react-icons/fa";
+import { FaPlane, FaLock, FaHeadset, FaSearch, FaHeart, FaEye, FaStar, FaStarHalf } from "react-icons/fa";
 
 import {Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
@@ -38,6 +38,28 @@ const Home = ()=>{
         },
         1024:{
             slidesPerView:4,
+        },
+        },
+        loop:true,
+        centeredSlides:true,
+        spaceBetween:10,
+    };
+    const swiperOptionsThree ={
+        breakpoints:{
+        0:{
+            slidesPerView:1,
+            spaceBetween:10,
+        },
+        450:{
+            slidesPerView:2,
+            spaceBetween:10,
+        },
+        768:{
+            slidesPerView:3,
+            spaceBetween:10,
+        },
+        1024:{
+            slidesPerView:3,
         },
         },
         loop:true,
@@ -299,6 +321,225 @@ const Home = ()=>{
                             </div>
                         </div>
                     </SwiperSlide>
+                </Swiper>
+            </div>
+        </section>
+
+        <section className="newsletter">
+            <form action="">
+                <h3>Subscribe for latest update</h3>
+                <input type="email" placeholder='enter your email' className='box'/>
+                <input type="submit" value="subscribe" className='btn'/>
+            </form>
+        </section>
+
+        <section className="arrivals" id="arrivals">
+            <div className="heading"><span>new arrivals</span></div>
+            <div className="Swiper">
+                <Swiper
+                watchSlidesProgress={true}
+                autoplay={{
+                    delay:3500,
+                    disableOnInteraction: false,
+                }}
+                modules={[Autoplay]}
+                className="arrivals-slider"
+                {...swiperOptionsThree}
+                >
+                <SwiperSlide>
+                    <a href="\#" className="box">
+                        <div className="image">
+                            <img src="images/book01.jfif" alt=""/>
+                        </div>
+                        <div className="content">
+                            <h3>new arrivals</h3>
+                            <div className="price">$15.99<span>$20.99</span></div>
+                            <div className="stars">
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStarHalf /></i>
+                            </div>
+                        </div>
+                    </a>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <a href="\#" className="box">
+                        <div className="image">
+                            <img src="images/book02.jfif" alt=""/>
+                        </div>
+                        <div className="content">
+                            <h3>new arrivals</h3>
+                            <div className="price">$15.99<span>$20.99</span></div>
+                            <div className="stars">
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStarHalf /></i>
+                            </div>
+                        </div>
+                    </a>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <a href="\#" className="box">
+                        <div className="image">
+                            <img src="images/book03.jfif" alt=""/>
+                        </div>
+                        <div className="content">
+                            <h3>new arrivals</h3>
+                            <div className="price">$15.99<span>$20.99</span></div>
+                            <div className="stars">
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStarHalf /></i>
+                            </div>
+                        </div>
+                    </a>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <a href="\#" className="box">
+                        <div className="image">
+                            <img src="images/book04.jpg" alt=""/>
+                        </div>
+                        <div className="content">
+                            <h3>new arrivals</h3>
+                            <div className="price">$15.99<span>$20.99</span></div>
+                            <div className="stars">
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStarHalf /></i>
+                            </div>
+                        </div>
+                    </a>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <a href="\#" className="box">
+                        <div className="image">
+                            <img src="images/book05.jfif" alt=""/>
+                        </div>
+                        <div className="content">
+                            <h3>new arrivals</h3>
+                            <div className="price">$15.99<span>$20.99</span></div>
+                            <div className="stars">
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStarHalf /></i>
+                            </div>
+                        </div>
+                    </a>
+                </SwiperSlide>
+                </Swiper>
+            </div>
+
+            <div className="Swiper">
+                <Swiper
+                watchSlidesProgress={true}
+                autoplay={{
+                    delay:3500,
+                    disableOnInteraction: false,
+                }}
+                modules={[Autoplay]}
+                className="arrivals-slider"
+                {...swiperOptionsThree}
+                >
+                <SwiperSlide>
+                    <a href="\#" className="box">
+                        <div className="image">
+                            <img src="images/book06.jfif" alt=""/>
+                        </div>
+                        <div className="content">
+                            <h3>new arrivals</h3>
+                            <div className="price">$15.99<span>$20.99</span></div>
+                            <div className="stars">
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStarHalf /></i>
+                            </div>
+                        </div>
+                    </a>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <a href="\#" className="box">
+                        <div className="image">
+                            <img src="images/book07.jpg" alt=""/>
+                        </div>
+                        <div className="content">
+                            <h3>new arrivals</h3>
+                            <div className="price">$15.99<span>$20.99</span></div>
+                            <div className="stars">
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStarHalf /></i>
+                            </div>
+                        </div>
+                    </a>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <a href="\#" className="box">
+                        <div className="image">
+                            <img src="images/book08.jpg" alt=""/>
+                        </div>
+                        <div className="content">
+                            <h3>new arrivals</h3>
+                            <div className="price">$15.99<span>$20.99</span></div>
+                            <div className="stars">
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStarHalf /></i>
+                            </div>
+                        </div>
+                    </a>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <a href="\#" className="box">
+                        <div className="image">
+                            <img src="images/book09.jpg" alt=""/>
+                        </div>
+                        <div className="content">
+                            <h3>new arrivals</h3>
+                            <div className="price">$15.99<span>$20.99</span></div>
+                            <div className="stars">
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStarHalf /></i>
+                            </div>
+                        </div>
+                    </a>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <a href="\#" className="box">
+                        <div className="image">
+                            <img src="images/book10.jpg" alt=""/>
+                        </div>
+                        <div className="content">
+                            <h3>new arrivals</h3>
+                            <div className="price">$15.99<span>$20.99</span></div>
+                            <div className="stars">
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStar /></i>
+                                <i><FaStarHalf /></i>
+                            </div>
+                        </div>
+                    </a>
+                </SwiperSlide>
                 </Swiper>
             </div>
         </section>
